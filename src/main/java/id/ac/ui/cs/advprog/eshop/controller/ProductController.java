59e37b02e224d4093aeb6f3f8d2584e7505ac95f
Peter Putra Lesmana <158464449+PeterP535@users.noreply.github.com>
2025-02-14 18:28:13 +0700

    tutor

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -13,30 +13,28 @@ import java.util.List;
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+
     @Autowired
     private ProductService service;
 
     @GetMapping("/create")
     public String createProductPage(Model model){
-        model.addAttribute("product", new Product());
-        return "product/create";
-    }
-
-    @PostMapping("/create")
-    public String createProduct(@ModelAttribute Product productM, Model model){
         Product product = new Product();
         model.addAttribute("product", product);
         return "createProduct";
     }
 
-    @PostMapping ("/list")
+    @PostMapping("/create")
+    public String createProduct(@ModelAttribute Product product, Model model){
+        service.create(product);
+        return "redirect:list";
+    }
+
+    @GetMapping ("/list")
     public String productListPage(Model model){
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
         return "productList";
     }
-
-
-
 
 }
