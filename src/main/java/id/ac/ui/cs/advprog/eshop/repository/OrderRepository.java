b12e101b0ece23a13a1a8c3e77c0c702826df470
Peter Putra Lesmana <158464449+PeterP535@users.noreply.github.com>
2025-03-07 17:25:34 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -7,10 +7,44 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Repository
 public class OrderRepository {
-    private List<Order> orderData = new ArrayList<>();
-    public Order save(Order order){return null;}
-    public Order findById(String id){return null;}
-    public List<Order> findAllByAuthor(String author){return null;}
 
+    private List<Order> orderData = new ArrayList<>();
+
+    public Order save(Order order) {
+        int i = 0;
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getId().equals(order.getId())) {
+                // Hapus order lama
+                orderData.remove(i);
+                // Tambahkan order baru
+                orderData.add(order);
+                return order;
+            }
+            i++;
+        }
+        // Jika belum ada data dengan ID tersebut, langsung tambahkan
+        orderData.add(order);
+        return order;
+    }
+
+    public Order findById(String id) {
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getId().equals(id)) {
+                return savedOrder;
+            }
+        }
+        return null;
+    }
+
+    public List<Order> findAllByAuthor(String author) {
+        List<Order> result = new ArrayList<>();
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getAuthor().equals(author)) {
+                result.add(savedOrder);
+            }
+        }
+        return result;
+    }
 }
